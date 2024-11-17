b75367e9d731746c91e8715dacdf6ea9d66e6207
Nicolo' Sartori <nsartori99@gmail.com>
2024-11-17 21:50:35 +0000

    feat: added username prompot

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# Setup the PSQL variable
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# Prompt the user for a username
+echo -n "Enter your username: "
+read USERNAME
+
+
